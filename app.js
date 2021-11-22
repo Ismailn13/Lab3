7baa4db9e524f3262d4a7fd04b9862deb24ffb1c
Ismailn13 <90825719+Ismailn13@users.noreply.github.com>
2021-11-22 14:27:27 +0400

    Added contacts page to main website

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index"); //no need for ejs extension
 });
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
